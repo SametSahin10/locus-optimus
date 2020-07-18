ebb624fe5cc228b4fd72a05cb316b18d90689500
Samet Sahin <a.sahin.ual@gmail.com>
2020-07-18 19:08:06 +0300

    add secondary and tertiary colors

--- lib/core/config/app_colors.dart
+++ lib/core/config/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primaryColor = Color(0xFFFFA69E);
+  static const secondaryColor = Color(0xFFDA8EA4);
+  static const tertiaryColor = Color(0xFFAB7CA1);
 }
