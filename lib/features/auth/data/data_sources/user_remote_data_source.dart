b5daa18b0453992006ffe762fbd52035261369d2
Samet Sahin <a.sahin.ual@gmail.com>
2020-07-18 19:09:33 +0300

    return null if cannot sign in or sign up

--- lib/features/auth/data/data_sources/user_remote_data_source.dart
+++ lib/features/auth/data/data_sources/user_remote_data_source.dart
@@ -10,23 +10,35 @@ class UserRemoteDataSourceImpl implements UserRemoteDataSource {
   Future<FirebaseUser> signIn(String email, String password) async {
     print("signing in");
     final auth = FirebaseAuth.instance;
-    final authResult = await auth.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    print("authResult.user: ${authResult.user}");
-    return authResult.user;
+    try {
+      final authResult = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print("user.uid: ${authResult.user.uid}");
+      return authResult.user;
+    } catch (err) {
+      print("Error occured while signing in");
+      print("error: $err");
+      return null;
+    }
   }
 
   @override
   Future<FirebaseUser> signUp(String email, String password) async {
-    print("signing in");
+    print("signing up");
     final auth = FirebaseAuth.instance;
-    final authResult = await auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    print("authResult.user: ${authResult.user}");
-    return authResult.user;
+    try {
+      final authResult = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print("user.uid: ${authResult.user.uid}");
+      return authResult.user;
+    } catch (err) {
+      print("Error occured while signing in");
+      print("error: $err");
+      return null;
+    }
   }
 }
